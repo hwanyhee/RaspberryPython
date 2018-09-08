07ba043879f25ea6014c7f8948a0f78cc54458bf
CCH <hwanyhee@naver.com>
2018-09-08 13:52:48 +0900

    111

---
+++ Dht11_Thingspeak.py
@@ -0,0 +1,28 @@
+
+import datetime, time
+import urllib.request
+import random
+import Adafruit_DHT
+
+
+
+pin = 4
+url = "https://api.thingspeak.com/update?api_key=AXIQQULTJUR031QJ&field1="
+
+   
+try:
+    while True:
+       
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)       
+        
+        if humidity is not None and temperature is not None:
+            f = urllib.request.urlopen(url+'Temp={0:0.1f}&Humidity={1:0.1f}'.format(temperature, humidity))
+            data = str(f.read())
+            print("data : ", data)
+            print('Temp={0:0.1f}&Humidity={1:0.1f}'.format(temperature, humidity))
+        else:
+            print('Failed to get reading. Try again!')
+        
+        time.sleep(10)
+except KeyboardInterrupt:
+    pass
