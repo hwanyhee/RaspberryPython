6d0d3b778896bd8722169d0d2de8ec6ae6a1d3e0
CCH <hwanyhee@naver.com>
2018-09-08 21:27:06 +0900

    sdsad

--- Sensor_Data_Grapth_Db.py
+++ Sensor_Data_Grapth_Db.py
@@ -1,24 +1,32 @@
-import datetime, time
-import urllib.request
 import Adafruit_DHT
+import pymysql
+
 
 sensor = Adafruit_DHT.DHT11
 pin = 4
 url = "http://192.168.0.7:8080/insert?temp="
 try:
+    
+    conn = pymysql.connect(host='localhost',user='iot', password='iot',db='iot_schema', port=3306,charset='utf8',cursorclass=pymysql.cursors.DictCursor)
+    cur = conn.cursor()  
+   
+                    
     while True:
        
         humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)       
         
         if humidity is not None and temperature is not None:
-            #데이타 보내기
-            f = urllib.request.urlopen(url+'{0:0.1f}&humi={1:0.1f}'.format(temperature, humidity))
-           
-            print('temp={0:0.1f}&humi={1:0.1f}'.format(temperature, humidity))
+          
+             cur.execute('INSERT INTO sensors(temperature,humidity) VALUES(?,?)', (temperature,humidity))
+             conn.commit()
         else:
             print('Failed to get reading. Try again!')       
        
         time.sleep(15)
 except KeyboardInterrupt:
     console.log('에러 발생')
+    
+finally:
+    conn.close()
+    cur.close()
     
