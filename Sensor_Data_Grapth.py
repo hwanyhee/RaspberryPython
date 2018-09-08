926802fac1fadcc5bb6bf91b3f12b3359466347f
CCH <hwanyhee@naver.com>
2018-09-08 20:39:25 +0900

    dfsdf

--- Sensor_Data_Grapth.py
+++ Sensor_Data_Grapth.py
@@ -5,7 +5,7 @@ import Adafruit_DHT
 
 sensor = Adafruit_DHT.DHT11
 pin = 4
-url = "http://192.168.0.7:8080/insert?"
+url = "http://192.168.0.7:8080/insert?temp="
 try:
     while True:
        
@@ -13,13 +13,13 @@ try:
         
         if humidity is not None and temperature is not None:
             #데이타 보내기
-            f = urllib.request.urlopen(url+'temp={0:0.1f}&humi={1:0.1f}'.format(temperature, humidity))
+            f = urllib.request.urlopen(url+'{0:0.1f}&humi={1:0.1f}'.format(temperature, humidity))
            
             print('temp={0:0.1f}&humi={1:0.1f}'.format(temperature, humidity))
         else:
             print('Failed to get reading. Try again!')       
        
-        time.sleep(5)
+        time.sleep(15)
 except KeyboardInterrupt:
     console.log('에러 발생')
     
