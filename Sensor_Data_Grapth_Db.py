9974e876f5865a75c9d7c102aeaa69bb26d3c1a6
CCH <hwanyhee@naver.com>
2018-09-08 21:48:21 +0900

    gfsd

--- Sensor_Data_Grapth_Db.py
+++ Sensor_Data_Grapth_Db.py
@@ -9,8 +9,8 @@ try:
     
     conn = pymysql.connect(host='localhost',user='iot', password='iot',db='iot_schema', port=3306,charset='utf8',cursorclass=pymysql.cursors.DictCursor)
     cur = conn.cursor()  
-   
-                    
+    print('cur'+cur)
+                   
     while True:
        
         humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)       
@@ -25,7 +25,7 @@ try:
         time.sleep(15)
 except KeyboardInterrupt:
     
-    console.log('�뿉�윭 諛쒖깮')    
+    print('�뿉�윭 諛쒖깮')    
 finally:
     conn.close()
     cur.close()
